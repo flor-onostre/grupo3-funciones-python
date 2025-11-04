a40bf84d7da1f7037c5108b8e5f7d6a395a7605c
Deniseae <denusz@live.com.ar>
2025-11-04 20:31:11 -0300

    ultimo arreglo

--- tests/test_promedioEsperon.py
+++ tests/test_promedioEsperon.py
@@ -1,5 +1,5 @@
-from funciones.PromedioEsperon import PromedioEsperon
+from funciones.PromedioEsperon import promedioesperon
 
 def test_promedioesperon():
     assert PromedioEsperon([2, 4, 6]) == 4
-    assert PromedioEsperon([]) is None
+    assert PromedioEsperon([]) is None 
