c39df38fdd708bf353744ecac53ec81b9206b984
Romero <nelson.romero@thomsonreuters.com>
2025-11-04 20:27:51 -0300

    Agrega funciones de Romero

--- tests/test_raiz_cuadrada_Romero.py
+++ tests/test_raiz_cuadrada_Romero.py
@@ -1,5 +1,5 @@
 from funciones.raiz_cuadrada_Romero import raiz_cuadrada
 
-def test_raiz_cuadrada():
+def test_raiz_cuadrada_Romero():
     assert raiz_cuadrada(9) == 3
     assert raiz_cuadrada(-4) is None
